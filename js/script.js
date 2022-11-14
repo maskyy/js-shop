4ba0fd5815d91758b520c2354f039b9181799e77
maskyy <62175286+maskyy@users.noreply.github.com>
2022-11-14 17:33:12 +0300

    Fix slider, get data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-var mySlider = new rSlider({
-  target: '#sampleSlider',
-  values: [10000, 1000000],
-  range: true,
-  tooltip: true,
-  scale: true,
-  labels: false,
-  step: 10000
-});
+'use strict';
+(async () => {
+  const fetchJson = async url => {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+  }
+  const mySlider = new rSlider({
+    target: '#sampleSlider',
+    values: {min: 10000, max: 1000000},
+    range: true,
+    tooltip: true,
+    scale: true,
+    labels: false,
+    step: 10000
+  });
+  console.log(mySlider);
+
+  const DATA_URL = 'https://main-shop-fake-server.herokuapp.com/db';
+  const data = await fetchJson(DATA_URL);
+  //console.table(data);
+
+
+})();
